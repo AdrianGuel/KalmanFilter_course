0ad94869308ddffaf4e54713d94d620f5fb7dd6b
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2024-02-09 19:49:50 +0000

    Update Cart_Kalman_Filter.m

--- session_3/Cart_Kalman_Filter.m
+++ session_3/Cart_Kalman_Filter.m
@@ -8,7 +8,7 @@ close all;
 b=1; m=2; k_m=5; Ts=1e-3; L=100;
 Cart_generative_model;
 
-Q=[w,w]*[w,w]';
+Q=1e-4*eye(2);
 R=v^2;
 P=1e4*eye(2);
 alpha=0.1;
@@ -17,7 +17,7 @@ x_pred=zeros([2,length(t)]);
 x_est=zeros([2,length(t)]);
 y_pred=zeros([1,length(t)]);
 
-x_pred(:,1)=[6,2.1]';
+x_pred(:,1)=[6,0]';
 for k=2:length(t)
     x_pred(:,k)=Ad*x_pred(:,k-1)+Bd*u;
     y_pred(k)=C*x_pred(:,k);
@@ -30,18 +30,28 @@ for k=2:length(t)
     x_est(:,k)=x_pred(:,k)+L*(y(k)-y_pred(k));
     P=P_pred-L*P_y*L';
     
-    % if k<100
+    % if k<3
     % Q=(1-alpha)*Q+alpha*L*(y(k)-y_pred(k))*(y(k)-y_pred(k))'*L';
     % end
 end
 
 subplot(2,1,1)
-plot(t,x(1,:),'b')
+yyaxis left
+plot(t,y,'b')
 hold on
-plot(t,y,'r')
-hold on
-plot(t,x_est(1,:),'k')
+plot(t,x_est(1,:),'k--')
+ylabel("position")
+yyaxis right
+plot(t,abs(x(1,:)-x_est(1,:)))
+ylabel("error")
+legend("true","estimation","error")
+
 subplot(2,1,2)
+yyaxis left
 plot(t,x(2,:),'b')
 hold on
-plot(t,x_est(2,:),'k')
+plot(t,x_est(2,:),'k--')
+ylabel("velocity")
+yyaxis right
+plot(t,abs(x(2,:)-x_est(2,:)),'k')
+ylabel("error")
